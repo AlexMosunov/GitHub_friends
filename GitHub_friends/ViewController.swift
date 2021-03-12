1826fba9df1f0218cfac740cbac039c699caaf7b
Oleksii Mosunov <alexmosunov@gmail.com>
2021-03-12 14:45:07 +0200

    Initial Commit

---
+++ GitHub_friends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub_friends
+//
+//  Created by Alex Mosunov on 12.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
